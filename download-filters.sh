77e99e469c29c2de269a9f9f7b5ad3bad1755d27
Minoplhy <kraneberru@disroot.org>
2021-02-25 19:56:50 +0700

    Not to be confused with Nextdns

--- download-filters.sh
+++ download-filters.sh
@@ -9,5 +9,5 @@ wget -O /etc/knot-resolver/list/energized-regional.rpz https://block.energized.p
 wget -O /etc/knot-resolver/list/energized-xtreme.rpz https://block.energized.pro/extensions/xtreme/formats/rpz.txt
 wget -O /etc/knot-resolver/list/blahdns.rpz https://oooo.b-cdn.net/blahdns/blahdns_rpz.txt
 wget -O /etc/knot-resolver/list/urlhaus-abuse_ch.rpz https://urlhaus.abuse.ch/downloads/rpz
-wget -O /etc/knot-resolver/list/nextdns-cname-cloaking.rpz https://github.com/minoplhy/dnsBlocklist/raw/main/rpz/nextdns-cname-cloaking.rpz
+wget -O /etc/knot-resolver/list/minop-cname-cloaking.rpz https://github.com/minoplhy/dnsBlocklist/raw/main/rpz/cname-cloaking.rpz
 exit
