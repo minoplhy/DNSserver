2f6fc89eeafc521625a10feddb9e4333d5671214
minoplhy <zebr@m21.kylz.nl>
2021-06-30 20:51:16 +0700

    Update with current configuration kresd.conf ->               * remove 1Hosts wildcards               * change 1Hosts to official link download-filters.sh ->                        * remove 1Hosts wildcards                        * change 1Hosts to official link

--- download-filters.sh
+++ download-filters.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-wget -O /etc/knot-resolver/list/minopallow.rpz https://git.kylz.nl/GitHub/DNSBlocklist/raw/branch/main/rpz/allowlist.rpz
+wget -O /etc/knot-resolver/list/minopallow.rpz https://kylz.nl/git/GitHub/DNSBlocklist/raw/branch/main/rpz/allowlist.rpz
 wget -O /etc/knot-resolver/list/adguard-exceptions.rpz https://sos-ch-dk-2.exo.io/noblt/RPZ/adguard/exceptions.txt
 wget -O /etc/knot-resolver/list/adguard-dns.rpz https://sos-ch-dk-2.exo.io/noblt/RPZ/adguard/dns.txt
 wget -O /etc/knot-resolver/list/minopdeny.rpz https://git.kylz.nl/GitHub/DNSBlocklist/raw/branch/main/rpz/denylist.rpz
@@ -8,8 +8,7 @@ wget -O /etc/knot-resolver/list/pgl-adserver.rpz https://pgl.yoyo.org/adservers/
 wget -O /etc/knot-resolver/list/energized-social.rpz https://block.energized.pro/extensions/social/formats/rpz.txt
 wget -O /etc/knot-resolver/list/energized-regional.rpz https://block.energized.pro/extensions/regional/formats/rpz.txt
 wget -O /etc/knot-resolver/list/energized-xtreme.rpz https://block.energized.pro/extensions/xtreme/formats/rpz.txt
-wget -O /etc/knot-resolver/list/1host-domains-pro.rpz https://sos-ch-dk-2.exo.io/noblt/RPZ/1Hosts/domains-pro.txt
-wget -O /etc/knot-resolver/list/1host-wildcards-pro.rpz https://sos-ch-dk-2.exo.io/noblt/RPZ/1Hosts/wildcards-pro.txt
+wget -O /etc/knot-resolver/list/1Hosts-pro.rpz https://badmojr.github.io/1Hosts/Pro/rpz.txt
 wget -O /etc/knot-resolver/list/urlhaus-abuse_ch.rpz https://urlhaus.abuse.ch/downloads/rpz
 wget -O /etc/knot-resolver/list/minop-cname-cloaking.rpz https://sos-ch-dk-2.exo.io/noblt/RPZ/adguard/cname-original.txt
 wget -O /etc/knot-resolver/list/ad-cname-tracker.rpz https://sos-ch-dk-2.exo.io/noblt/RPZ/adguard/cname-tracker.txt
